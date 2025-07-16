49ffe0007f80c7bc1af4f0ad16b6c4b1a666e806
ChinoLim <chino27navarro@gmail.com>
2025-07-16 11:57:14 +0800

    Added login to most recent branch(stand by for rest of changes)

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,41 @@
 import express from "express";
 import * as mysql from "../model/userModel.js";
+import argon2 from 'argon2';
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+
+router.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await mysql.getUserByUsername(username);
+        if (!user) {
+        return res.status(400).json({ message: "Invalid username or password" });
+        }
+        console.log("User from DB:", user);
+
+        const isValid = await argon2.verify(user.userPassword, password);
+        console.log("isValid:", isValid);
+
+        if (!isValid) {
+        return res.status(400).json({ message: "Invalid username or password" });
+        }
+        
+        //Success
+        const token = jwt.sign(
+        { userId: user.userId, role: user.userRole },
+        "secretkey", //move to .env l8r
+        { expiresIn: "1h" }
+        );
+
+        console.log("Login success! Sending token...");
+        return res.json({ message: "Login successful", token });
+
+        } catch (err) {
+            console.error("Login error:", err);
+            res.status(500).json({ message: "Internal server error" });
+        }
+}); //test: curl -X POST http://localhost:5000/api/login   -H "Content-Type: application/json"   -d '{"username":"janedoe","password":"secret123"}'
 
 router.post("/createUser", async(req, res) =>{
     try{
