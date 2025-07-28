a4c5c54c2b4f73db1cc54d85c46b8c843714a440
ShrekIsGoat <henry_villanueva@dlsu.edu.ph>
2025-07-28 15:51:22 +0800

    Add files via upload

---
+++ public/FINAL_populate_users.sql
@@ -0,0 +1,8 @@
+  INSERT INTO Users (fullName, userRole, username, userPassword, pathName, dateAdded, lastEditedDate, lastEditedUser, deleteFlag)
+VALUES
+('Ching Wong Man', 'admin', 'PaddleMan365', 'WompWomp', NULL, 2024-01-01, '2025-07-28 05:00:51', 1, 0),
+('Libro James', 'manager', 'LBJ', 'WowWowWee', NULL, 2024-01-01, '2025-07-22 05:00:51', 1, 0);
+('Larry Legend', 'staff', 'BirdMan', 'MuscleMan', NULL, 2024-07-01, '2025-05-27 05:00:51', 1, 0);
+('Justin Beaver', 'staff', 'PopstarB', 'Hatdog', NULL, 2024-07-01, '2025-05-28 05:00:51', 1, 0);
+('Jiminny Cricket', 'auditor', 'SkibidiToilet', 'YepYep', NULL, 2024-01-01, '2025-06-28 05:00:51', 1, 0);
+
