02c20905f25450fe5d7c3c78bc6f44879125930a
aaaaandreiii <andrei.balingit.alab1@gmail.com>
2025-08-04 14:39:20 +0800

    test: undid export app

--- backend/app.js
+++ backend/app.js
@@ -59,8 +59,6 @@ app.use('/api/dashboard', dashboardRouter);
 
 bootstrapAdminUser().catch(console.error);
 
-// app.listen(PORT, () => {
-//   console.log(`Backend listening on http://localhost:${PORT}`);
-// });
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`Backend listening on http://localhost:${PORT}`);
+});
